61d82a5663a49cb72266364a45d7b5984f0ef0cf
harryt1002 <h3tang@ucsd.edu>
2022-10-28 22:28:00 -0700

    tried my best

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,12 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const airHorn = document.querySelector('air-horn');
+  airHorn.addEventListener('click', (event) => {
+    const imgAH = new Image();
+    imgAH.src = 'Lab5_Starter/assets/images/air-horn.svg'
+    // imgAH.addEventListener('load', () => {
+
+    // }, false);
+  });
 }
